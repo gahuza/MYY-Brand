fdf027eb2f892860a9ddea4fa2098fcbcc94f43b
gahuza <gahuzaj12@gmail.com>
2023-02-15 13:00:18 +0200

    circleciad

--- src/--test--/Blog.test.js
+++ src/--test--/Blog.test.js
@@ -89,33 +89,6 @@ describe("create /api/query", () => {
  
   //delete
 
- 
-
-// import request from "supertest";
-// import mongoose from "mongoose";
-// import app from "../app.js";
-// import {expect, jest, test} from '@jest/globals';
-// import dotenv from "dotenv";
-// import Blog from "../models/Blogs.model.js";
-// dotenv.config({ path: "../.env" });
-// /* Connecting to the database before each test. */
-// jest.setTimeout(10000);
-
-// beforeEach(async () => {
-//     await mongoose.connect(process.env.MONGO_URI_TEST, { useNewUrlParser: true });
-//   });
-//   /* Closing database connection after each test. */
-//   afterEach(async () => {
-//     await mongoose.connection.close();
-//   });
-
-//   describe('GET /api/blogs', () => {
-//     it('get blogs', async () => {
-//       const res = await request(app).get('/api/blogs');
-//       expect(res.status).toBe(208);
-//     });  })
-
-
 
 //delete query
 
@@ -199,7 +172,45 @@ describe("UPDATE /api/blogs/:id", () => {
 
     //get single blog
 
+   //add comment
+   describe('POST /blogs/:id/comments', () => {
+    it('return a 401 status if user is not logged in', async () => {
+      const res = await request(app).post('/api/blogs/').send({});
+      expect(res.status).toEqual(500);
+    });
   
-  
-    
    
+  })
+  describe("GET /api/comment", () => {
+    test('should respond with a 200 status code', async () => {
+      const response = await request(app).get("/api/comments/63e373de2ffaef80bcc03c95")
+      expect(response.statusCode).toBe(200)
+    })
+  
+  })
+
+
+   //add or remove like
+   describe('POST /blogs/:id/likes', () => {
+    it('return a 401 status if user is not logged in', async () => {
+      const res = await request(app).post('/api/blogs/').send({});
+      expect(res.status).toEqual(401);
+    });
+    it("return a 400 status if blog doesn't exist", async () => {
+      const user = {
+        email: 'gahuza@gmail.com',
+        password: 'gahuza',
+      };
+
+      const login = await request(app).post('/api/login').send(user);
+      const token = login.body.token;
+      const allBlogs = await request(app).get('/api/blogs');
+      const currentBlog = allBlogs.body.data[0];
+      const id = currentBlog._id;
+      const updatedBlog = await request(app)
+        .post(`/api/${id + 1}/likes`)
+        .set('auth' + token);
+      expect(updatedBlog.status).toEqual(400);
+      const message = updatedBlog.body.message;
+      expect(message).toEqual("Blog doesn't exist!");
+    });})
