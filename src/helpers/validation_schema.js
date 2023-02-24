a40f173eacb3b139573bb98ae015257b50c04cec
gahuza <gahuzaj12@gmail.com>
2023-02-24 15:47:06 +0200

    bacckend ircleci update

--- src/helpers/validation_schema.js
+++ src/helpers/validation_schema.js
@@ -1,36 +1,36 @@
-const { ref } = require('joi')
-const joi=require('joi')
+import { ref } from 'joi'
+import { object, string, ref as _ref } from 'joi'
 
-const blogSchema=joi.object({
-    title:joi.string().min(10).required(),
-    body:joi.string().min(20).required()
+const blogSchema=object({
+    title:string().min(10).required(),
+    body:string().min(20).required()
 })
 
-const updateBlogSchema=joi.object({
-    Blog_id:joi.string().required(),
-    title:joi.string().min(10),
-    body:joi.string().min(20)
+const updateBlogSchema=object({
+    Blog_id:string().required(),
+    title:string().min(10),
+    body:string().min(20)
 })
 
-const createUserSchema=joi.object({
-    username:joi.string().required().min(4),
-    email:joi.string().email().required(), 
-    password:joi.string().min(6).required(),
-    confirm_password:joi.ref('password'),
+const createUserSchema=object({
+    username:string().required().min(4),
+    email:string().email().required(), 
+    password:string().min(6).required(),
+    confirm_password:_ref('password'),
 })
-const loginUserSchema=joi.object({
-    email:joi.string().email().required(),
-    password:joi.string().min(6).required(),
+const loginUserSchema=object({
+    email:string().email().required(),
+    password:string().min(6).required(),
 })
-const messageSchema=joi.object({
-    name:joi.string().required(),
-    email:joi.string().email().required(),
-    message:joi.string().min(6).required(),
-    subject:joi.string().min(6).required(),
-    phone:joi.string().min(6).required(),
+const messageSchema=object({
+    name:string().required(),
+    email:string().email().required(),
+    message:string().min(6).required(),
+    subject:string().min(6).required(),
+    phone:string().min(6).required(),
 
 })
 
-module.exports={
+export default{
     blogSchema,updateBlogSchema,createUserSchema,loginUserSchema, messageSchema
 }
