b5c3296bdd1880b504941d8f36705807690b3a6a
gahuza <gahuzaj12@gmail.com>
2023-02-27 14:23:54 +0200

    backend ain update

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import swaggerUi from 'swagger-ui-express'
 import  docrouter  from "./documentation/swagger.doc.js";
 
 const app = express();
-app.use(cors())
+app.use(cors(corsOpts))
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 app.use(express.json());
